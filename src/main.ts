815b53322c97092995cbfd5e82c85946129f3b8f
Cleison Ferreira <cleisonferreira@gmail.com>
2024-05-30 12:28:32 -0300

    Install Class Transformer

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 
@@ -9,6 +9,8 @@ async function bootstrap()
   const app = await NestFactory.create(AppModule);
   
   Logger.log("Server running http://localhost:" + port,"Bootstrap");
+
+  app.useGlobalPipes(new ValidationPipe({whitelist:true}));
 
   await app.listen(port);
 }
